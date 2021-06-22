3290409b8a818d3cdd78d30e6cfbceb76cead1ed
Alberto Diaz <alberto.diazolivar@externos-cl.cencosud.com>
2021-06-22 12:37:29 -0400

    fix: typo

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="not-found">
-    <h1 style="text-align: center;">No se encontraron perosnajes :(</h1>
+    <h1 style="text-align: center;">No se encontraron personajes :(</h1>
   </div>
 </template>
